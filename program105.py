febe7245011f97683603c307652bf8e64a7e0777
BxtGeek <manokumar2684@gmail.com>
2023-11-27 01:51:24 +0530

    changes

---
+++ program105.py
@@ -0,0 +1,12 @@
+def main():
+    print_first_name("Manoj")
+    print_last_name("Kumar")
+    
+def print_first_name(x):
+    print(f"Hello {x}")
+
+def print_last_name(y):
+    print(f"Hello {y}")
+    
+if __name__ == "__main__":
+    main()
