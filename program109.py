4dfa7145224db98a5326f25c14d8542d021fed8d
BxtGeek <manokumar2684@gmail.com>
2023-11-28 23:27:29 +0530

    changes

--- program109.py
+++ program109.py
@@ -1,1 +1,12 @@
 # Write a program to find the sum of the given series 1+2+3+ . . . . . .(N terms) 
+
+nums= []
+count = int(input("Enter the value you need in series:"))
+for i in range(count):
+    nums.append(int(input(f"Enter the {i} element:")))
+
+series_sum = 0
+for i in range(count):
+    series_sum = series_sum + nums[i]
+
+print("Sum of Series is",series_sum)
